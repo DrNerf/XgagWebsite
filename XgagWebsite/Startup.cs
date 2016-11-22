b2a30d13ea347aa7b9cdc07bf88d72d1ea03764f
Svetoslav Todorov <drnerf95@gmail.com>
2016-11-22 21:22:33 +0200

    Initial commit.

---
+++ XgagWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XgagWebsite.Startup))]
+namespace XgagWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
