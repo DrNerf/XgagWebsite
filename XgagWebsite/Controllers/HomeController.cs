346a40a3eb64f93bbf1c4a0b6af8ee893bba9dbd
Svetoslav Todorov <drnerf95@gmail.com>
2016-11-23 19:43:27 +0200

    Posts upload done.

--- XgagWebsite/Controllers/HomeController.cs
+++ XgagWebsite/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using XgagWebsite.Models;
 
 namespace XgagWebsite.Controllers
 {
@@ -10,7 +12,9 @@ namespace XgagWebsite.Controllers
     {
         public ActionResult Index()
         {
-            var posts = DbContext.Posts.ToList();
+            var posts = DbContext.Posts
+                .OrderByDescending(p => p.DateCreated)
+                .ToList();
 
             return View(posts);
         }
@@ -27,6 +31,48 @@ namespace XgagWebsite.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [Authorize]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult> UploadPost(Post post)
+        {
+            var isSuccess = false;
+            var isValid = false;
+            string message = string.Empty;
+
+            if (!String.IsNullOrEmpty(post.Title) && !String.IsNullOrEmpty(post.ImageUrl))
+            {
+                isValid = true;
+            }
+            else
+            {
+                message = "Invalid data!";
+            }
+
+            if (isValid)
+            {
+                try
+                {
+                    post.DateCreated = DateTime.Now;
+                    post.Score = 0;
+                    DbContext.Posts.Add(post);
+                    await DbContext.SaveChangesAsync();
+                    isSuccess = true;
+                }
+                catch (Exception ex) 
+                {
+                    message = ex.Message;
+                }
+            }
+
+            return Json(new { IsSuccess = isSuccess, Message = message });
         }
     }
 }
