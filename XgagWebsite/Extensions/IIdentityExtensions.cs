53935602f4b6c1024da818588fb56e6e01630fee
Svetoslav <drnerf95@gmail.com>
2017-01-29 13:26:05 +0200

    Remaining votes added.

---
+++ XgagWebsite/Extensions/IIdentityExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Security.Principal;
+using XgagWebsite.Enums;
+using XgagWebsite.Helpers;
+using XgagWebsite.Models;
+
+namespace XgagWebsite
+{
+    public static class IIdentityExtensions
+    {
+        public static int GetRemainingVotes(this IIdentity identity, VoteType voteType)
+        {
+            var result = 0;
+            var maxVotes = ConfigurationHelper.Instance.MaxDailyVotes;
+
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                var user = db.Users
+                    .First(x => x.UserName == identity.Name);
+
+                result = maxVotes - user.DailyVotes
+                    .Count(x => x.VoteDate == DateTime.Now.Date && x.VoteType == voteType);
+            }
+
+            return result;
+        }
+    }
+}
