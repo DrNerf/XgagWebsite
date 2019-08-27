3d25f5eb680fe51a33fdbebe9be80e58cc636849
Svetoslav Todorov <drnerf95@gmail.com>
2019-08-27 22:20:27 +0300

    New comments UI wired.

---
+++ XgagWebsite/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace XgagWebsite
+{
+    public static class DateTimeExtensions
+    {
+        public static string GetDateString(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd/MM/yyyy");
+        }
+
+        public static string GetTimeString(this DateTime dateTime)
+        {
+            return dateTime.ToString("HH:mm");
+        }
+    }
+}
